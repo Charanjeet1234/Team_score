f9ce1295f42c7e30709b18e3217a4a8ce617ea8a
Charanjeet <cjeet336@gmail.com>
2024-02-05 16:49:56 +0400

    Added reterive data from localstorage

--- result-script.js
+++ result-script.js
@@ -4,12 +4,24 @@ const skitTeam1 = localStorage.getItem("skitTeam1");
 const shakeItTeam1 = localStorage.getItem("shakeItTeam1");
 const quizTeam1 = localStorage.getItem("quizTeam1");
 
+// Display the result and the calculations in the result page
+document.getElementById("teambuildingT1").innerText = teamBuildingT1;
+document.getElementById("skitTeam1").innerText = skitTeam1;
+document.getElementById("stretchTeam1").innerText = shakeItTeam1;
+document.getElementById("quizTeam1").innerText = quizTeam1;
+
 ////////////////////////////// Team 2 //////////////////////////
 
 const teamBuildingT2 = localStorage.getItem("teamBuildingT2");
 const skitTeam2 = localStorage.getItem("skitTeam2");
 const shakeItTeam2 = localStorage.getItem("shakeItTeam2");
 const quizTeam2 = localStorage.getItem("quizTeam2");
+
+// Display the result and the calculations in the result page
+document.getElementById("teamBuildingT2").innerText = teamBuildingT2;
+document.getElementById("skitTeam2").innerText = skitTeam2;
+document.getElementById("shakeItTeam2").innerText = shakeItTeam2;
+document.getElementById("quizTeam2").innerText = quizTeam2;
 
 //////////////////////////////// Team 3 //////////////////////////
 
@@ -18,12 +30,24 @@ const skitTeam3 = localStorage.getItem("skitTeam3");
 const shakeItTeam3 = localStorage.getItem("shakeItTeam3");
 const quizTeam3 = localStorage.getItem("quizTeam3");
 
+// Display the result and the calculations in the result page
+document.getElementById("teamBuildingT3").innerText = teamBuildingT3;
+document.getElementById("skitTeam3").innerText = skitTeam3;
+document.getElementById("shakeItTeam3").innerText = shakeItTeam3;
+document.getElementById("quizTeam3").innerText = quizTeam3;
+
 ////////////////////////////// Team 4 //////////////////////////
 
 const teamBuildingT4 = localStorage.getItem("teamBuildingT4");
 const skitTeam4 = localStorage.getItem("skitTeam4");
 const shakeItTeam4 = localStorage.getItem("shakeItTeam4");
 const quizTeam4 = localStorage.getItem("quizTeam4");
+
+// Display the result and the calculations in the result page
+document.getElementById("teamBuildingT4").innerText = teamBuildingT4;
+document.getElementById("skitTeam4").innerText = skitTeam4;
+document.getElementById("shakeItTeam4").innerText = shakeItTeam4;
+document.getElementById("quizTeam4").innerText = quizTeam4;
 
 ////////////////////////////////// Team 5 //////////////////////////
 
@@ -32,6 +56,12 @@ const skitTeam5 = localStorage.getItem("skitTeam5");
 const shakeItTeam5 = localStorage.getItem("shakeItTeam5");
 const quizTeam5 = localStorage.getItem("quizTeam5");
 
+// Display the result and the calculations in the result page
+document.getElementById("teamBuildingT5").innerText = teamBuildingT5;
+document.getElementById("skitTeam5").innerText = skitTeam5;
+document.getElementById("shakeItTeam5").innerText = shakeItTeam5;
+document.getElementById("quizTeam5").innerText = quizTeam5;
+
 ///////////////////////////////// Team 6 //////////////////////////
 
 const teamBuildingT6 = localStorage.getItem("teamBuildingT6");
@@ -39,6 +69,11 @@ const skitTeam6 = localStorage.getItem("skitTeam6");
 const shakeItTeam6 = localStorage.getItem("shakeItTeam6");
 const quizTeam6 = localStorage.getItem("quizTeam6");
 
+// Display the result and the calculations in the result page
+document.getElementById("teamBuildingT6").innerText = teamBuildingT6;
+document.getElementById("skitTeam6").innerText = skitTeam6;
+document.getElementById("shakeItTeam6").innerText = shakeItTeam6;
+document.getElementById("quizTeam6").innerText = quizTeam6;
 ///////////////////////////////// Team 7 //////////////////////////
 
 const teamBuildingT7 = localStorage.getItem("teamBuildingT7");
@@ -46,12 +81,22 @@ const skitTeam7 = localStorage.getItem("skitTeam7");
 const shakeItTeam7 = localStorage.getItem("shakeItTeam7");
 const quizTeam7 = localStorage.getItem("quizTeam7");
 
+// Display the result and the calculations in the result page
+document.getElementById("teamBuildingT7").innerText = teamBuildingT7;
+document.getElementById("skitTeam7").innerText = skitTeam7;
+document.getElementById("shakeItTeam7").innerText = shakeItTeam7;
+document.getElementById("quizTeam7").innerText = quizTeam7;
 ////////////////////////////////// Team 8 //////////////////////////
 
 const teamBuildingT8 = localStorage.getItem("teamBuildingT8");
 const skitTeam8 = localStorage.getItem("skitTeam8");
 const shakeItTeam8 = localStorage.getItem("shakeItTeam8");
 const quizTeam8 = localStorage.getItem("quizTeam8");
+// Display the result and the calculations in the result page
+document.getElementById("teamBuildingT8").innerText = teamBuildingT8;
+document.getElementById("skitTeam8").innerText = skitTeam8;
+document.getElementById("shakeItTeam8").innerText = shakeItTeam8;
+document.getElementById("quizTeam8").innerText = quizTeam8;
 
 ////////////////////////////////// Team 9 //////////////////////////
 
@@ -60,3 +105,46 @@ const skitTeam9 = localStorage.getItem("skitTeam9");
 const shakeItTeam9 = localStorage.getItem("shakeItTeam9");
 const quizTeam9 = localStorage.getItem("quizTeam9");
 
+// Display the result and the calculations in the result page
+document.getElementById("teamBuildingT9").innerText = teamBuildingT9;
+document.getElementById("skitTeam9").innerText = skitTeam9;
+document.getElementById("shakeItTeam9").innerText = shakeItTeam9;
+document.getElementById("quizTeam9").innerText = quizTeam9;
+
+// /////////////// Team 10 //////////////////////////
+const teamBuildingT10 = localStorage.getItem("teamBuildingT10");
+const skitTeam10 = localStorage.getItem("skitTeam10");
+const shakeItTeam10 = localStorage.getItem("shakeItTeam10");
+const quizTeam10 = localStorage.getItem("quizTeam10");
+
+// Display the result and the calculations in the result page
+document.getElementById("teamBuildingT10").innerText = teamBuildingT10;
+document.getElementById("skitTeam10").innerText = skitTeam10;
+document.getElementById("shakeItTeam10").innerText = shakeItTeam10;
+document.getElementById("quizTeam10").innerText = quizTeam10;
+
+////////////////////////// Team 11 //////////////////////////
+
+const teamBuildingT11 = localStorage.getItem("teamBuildingT11");
+const skitTeam11 = localStorage.getItem("skitTeam11");
+const shakeItTeam11 = localStorage.getItem("shakeItTeam11");
+const quizTeam11 = localStorage.getItem("quizTeam11");
+
+// Display the result and the calculations in the result page
+document.getElementById("teamBuildingT11").innerText = teamBuildingT11;
+document.getElementById("skitTeam11").innerText = skitTeam11;
+document.getElementById("shakeItTeam11").innerText = shakeItTeam11;
+document.getElementById("quizTeam11").innerText = quizTeam11;
+
+////////////////////////// Team 12 //////////////////////////
+
+const teamBuildingT12 = localStorage.getItem("teamBuildingT12");
+const skitTeam12 = localStorage.getItem("skitTeam12");
+const shakeItTeam12 = localStorage.getItem("shakeItTeam12");
+const quizTeam12 = localStorage.getItem("quizTeam12");
+
+// Display the result and the calculations in the result page
+document.getElementById("teamBuildingT12").innerText = teamBuildingT12;
+document.getElementById("skitTeam12").innerText = skitTeam12;
+document.getElementById("shakeItTeam12").innerText = shakeItTeam12;
+document.getElementById("quizTeam12").innerText = quizTeam12;
