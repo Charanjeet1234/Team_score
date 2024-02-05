c9ca56a6a2ee9279dda6d026c7f70eb90712defd
Charanjeet <cjeet336@gmail.com>
2024-02-05 14:56:30 +0400

    updated+

---
+++ result-script.js
@@ -0,0 +1,62 @@
+// reterive data from the local storage
+const teamBuildingT1 = localStorage.getItem("teamBuildingT1");
+const skitTeam1 = localStorage.getItem("skitTeam1");
+const shakeItTeam1 = localStorage.getItem("shakeItTeam1");
+const quizTeam1 = localStorage.getItem("quizTeam1");
+
+////////////////////////////// Team 2 //////////////////////////
+
+const teamBuildingT2 = localStorage.getItem("teamBuildingT2");
+const skitTeam2 = localStorage.getItem("skitTeam2");
+const shakeItTeam2 = localStorage.getItem("shakeItTeam2");
+const quizTeam2 = localStorage.getItem("quizTeam2");
+
+//////////////////////////////// Team 3 //////////////////////////
+
+const teamBuildingT3 = localStorage.getItem("teamBuildingT3");
+const skitTeam3 = localStorage.getItem("skitTeam3");
+const shakeItTeam3 = localStorage.getItem("shakeItTeam3");
+const quizTeam3 = localStorage.getItem("quizTeam3");
+
+////////////////////////////// Team 4 //////////////////////////
+
+const teamBuildingT4 = localStorage.getItem("teamBuildingT4");
+const skitTeam4 = localStorage.getItem("skitTeam4");
+const shakeItTeam4 = localStorage.getItem("shakeItTeam4");
+const quizTeam4 = localStorage.getItem("quizTeam4");
+
+////////////////////////////////// Team 5 //////////////////////////
+
+const teamBuildingT5 = localStorage.getItem("teamBuildingT5");
+const skitTeam5 = localStorage.getItem("skitTeam5");
+const shakeItTeam5 = localStorage.getItem("shakeItTeam5");
+const quizTeam5 = localStorage.getItem("quizTeam5");
+
+///////////////////////////////// Team 6 //////////////////////////
+
+const teamBuildingT6 = localStorage.getItem("teamBuildingT6");
+const skitTeam6 = localStorage.getItem("skitTeam6");
+const shakeItTeam6 = localStorage.getItem("shakeItTeam6");
+const quizTeam6 = localStorage.getItem("quizTeam6");
+
+///////////////////////////////// Team 7 //////////////////////////
+
+const teamBuildingT7 = localStorage.getItem("teamBuildingT7");
+const skitTeam7 = localStorage.getItem("skitTeam7");
+const shakeItTeam7 = localStorage.getItem("shakeItTeam7");
+const quizTeam7 = localStorage.getItem("quizTeam7");
+
+////////////////////////////////// Team 8 //////////////////////////
+
+const teamBuildingT8 = localStorage.getItem("teamBuildingT8");
+const skitTeam8 = localStorage.getItem("skitTeam8");
+const shakeItTeam8 = localStorage.getItem("shakeItTeam8");
+const quizTeam8 = localStorage.getItem("quizTeam8");
+
+////////////////////////////////// Team 9 //////////////////////////
+
+const teamBuildingT9 = localStorage.getItem("teamBuildingT9");
+const skitTeam9 = localStorage.getItem("skitTeam9");
+const shakeItTeam9 = localStorage.getItem("shakeItTeam9");
+const quizTeam9 = localStorage.getItem("quizTeam9");
+
